ec0639d1f1cefe620bb77979ca427ab25878303e
Ko Takagi <knockoutmarch@gmail.com>
2019-11-19 13:33:15 +0900

    :+1: Add table

---
+++ database/seeds/ReviewsTableSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class ReviewsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+	    DB::insert( "INSERT INTO reviews (book_id,name,body) VALUES
+			('1','ichiro','Good book!'),
+			('1','hideki','Awesome!'),
+			('2','daisuke','Cool!')
+		" );
+    }
+}
